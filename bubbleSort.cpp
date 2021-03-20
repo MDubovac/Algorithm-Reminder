d41bec8071fcdf7385df23166d95ff6f7d6129d3
MDubovac <54212992+MDubovac@users.noreply.github.com>
2021-03-20 15:28:17 +0100

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,15 +2,11 @@
 
 using namespace std;
 
-void bubbleSort(double arr[], int n)
-{
+void bubbleSort(double arr[], int n){
     int j, i;
-    for(j = 0; j < n - 1; j++)
-    {
-        for(i = 0; i < n - j - 1; i++)
-        {
-            if(arr[i] > arr[i+1])
-            {
+    for(j = 0; j < n - 1; j++){
+        for(i = 0; i < n - j - 1; i++){
+            if(arr[i] > arr[i+1]){
                 double temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
@@ -19,18 +15,15 @@ void bubbleSort(double arr[], int n)
     }
 }
 
-void printArray(double arr[], int size)
-{
+void printArray(double arr[], int size){
     int i;
-    for(i = 0; i < size; i++)
-    {
+    for(i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main()
-{
+int main() {
     // Test
     double speed[] = {123.4, 211.2, 220.5, 167.4, 189.5, 192.2};
     int n = sizeof(speed) / sizeof(speed[0]);
