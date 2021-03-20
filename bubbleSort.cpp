9bebd1bd6010bab50ebae0f9fc96f1abc9dce4d1
MDubovac <54212992+MDubovac@users.noreply.github.com>
2021-03-20 15:06:28 +0100

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,11 +2,15 @@
 
 using namespace std;
 
-void bubbleSort(double arr[], int n){
+void bubbleSort(double arr[], int n)
+{
     int j, i;
-    for(j = 0; j < n - 1; j++){
-        for(i = 0; i < n - j - 1; i++){
-            if(arr[i] > arr[i+1]){
+    for(j = 0; j < n - 1; j++)
+    {
+        for(i = 0; i < n - j - 1; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
                 double temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
@@ -15,9 +19,11 @@ void bubbleSort(double arr[], int n){
     }
 }
 
-void printArray(double arr[], int size){
+void printArray(double arr[], int size)
+{
     int i;
-    for(i = 0; i < size; i++){
+    for(i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
